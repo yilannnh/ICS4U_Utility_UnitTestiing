9868ab90142fdaedbd22ba7c59239006f06f1bfc
yilannnh <yilan.hong23@ycdsbk12.ca>
2022-09-25 11:21:11 -0400

    first commit :D

---
+++ src/UtilityTUnitTesting/Main.java
@@ -0,0 +1,13 @@
+package UtilityTUnitTesting;
+
+import UtilityTUnitTesting.MathUtility;
+
+public class Main{
+    public static void main (String[] args) {
+        System.out.println("This is the main method");
+    }
+
+    public static int add (int intNum1, int intNum2) {
+        return intNum1 + intNum2;
+    }
+}
---
+++ src/UtilityTUnitTesting/MathUtilityTest.java
@@ -0,0 +1,3 @@
+package UtilityTUnitTesting;
+
+//port static org.junit.jupiter
